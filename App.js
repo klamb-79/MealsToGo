d3fc3c212ada8aa421a98b6cf809a62ec6c7488c
klamb79 <keronlamb@gmail.com>
2023-07-28 16:20:59 -0600

    New content

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar, setStatusBarBackgroundColor } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.search}>
+        <Text>search</Text>
+      </View >
+      <View style={styles.foodlist}>
+        <Text style={{padding: 15}}>list</Text>
+      </View >
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
+   flex: 1,
+   //justifyContent: "center",
+   backgroundColor: "white",
+   paddingTop:20,
+  //  paddingBottom: 20
   },
+  search:{
+    //flexDirection: 'column',
+    width: 400,
+    justifyContent: "center",
+    //height: 30,
+    padding: 15,
+    backgroundColor: "green"
+
+  },
+  foodlist:{
+    flex: 1,
+    width: 400,
+    height: 100,
+    backgroundColor: "blue"
+  }
 });
