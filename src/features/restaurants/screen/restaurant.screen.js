b83a699109807de1fa46ed7e884d297c9249fd60
klamb79 <keronlamb@gmail.com>
2023-09-08 16:16:30 -0600

    update

--- src/features/restaurants/screen/restaurant.screen.js
+++ src/features/restaurants/screen/restaurant.screen.js
@@ -1,27 +1,26 @@
 import React from "react";
 import styled from "styled-components/native";
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { RestaurantInfo } from "../components/restaurant-info.componet";
 
-const RestaurantSearch = styled(View)`
+const RestaurantSearch = styled.View`
   width: 410px;
   background-color: ${(props) => props.theme.colors.bg.secondary};
   justify-content: center;
   padding: ${(props) => props.theme.space[2]};
 `;
-const RestaurantFoodList = styled(View)`
+const RestaurantFoodList = styled.View`
   flex: 1;
   width: 410px;
-  height: 100px;
+  height: 5px;
   background-color: ${(props) => props.theme.colors.bg.secondary};
-  padding: ${(props) => props.theme.space[3]};
+  padding: ${(props) => props.theme.space[2]};
 `;
-const RestaurantTitle = styled(Text)`
+const RestaurantTitle = styled.Text`
   justify-content: center;
   text-align: center;
-  padding-top: ${(props) => props.theme.space[3]};
+  padding-top: ${(props) => props.theme.space[4]};
 `;
 
 export const RestaurantScreen = () => (
@@ -32,7 +31,27 @@ export const RestaurantScreen = () => (
     </RestaurantSearch>
 
     <RestaurantFoodList>
-      <RestaurantInfo />
+      <FlatList
+        data={[
+          { name: 1 },
+          { name: 2 },
+          { name: 3 },
+          { name: 4 },
+          { name: 5 },
+          { name: 6 },
+          { name: 7 },
+          { name: 8 },
+          { name: 9 },
+          { name: 10 },
+          { name: 11 },
+          { name: 12 },
+          { name: 13 },
+          { name: 14 },
+        ]}
+        renderItem={() => <RestaurantInfo />}
+        keyExtractor={(item) => item.name}
+        //contentContainerStye={{ padding: 16 }}
+      />
     </RestaurantFoodList>
   </>
 );
